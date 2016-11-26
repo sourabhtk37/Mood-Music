e0c20e5c209324d9a1e05fcb85ee3086ab2eecc4
T K Sourabh <sourabhtk37@gmail.com>
2016-11-27 02:08:40 +0530

    running

--- moodmusic/moodanalyser/urls.py
+++ moodmusic/moodanalyser/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^recieve_url/$',views.sentiment_analyser),
-    url(r'^deploy_detail/$',views.deploy_detail)
+    url(r'^deploy_detail/$',views.deploy_detail),
 ]
